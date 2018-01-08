45401ce2f58d6e92fdbdaf6b5932e770ce4a77d7
Jeremy Nelson <jermnelson@gmail.com>
2018-01-08 10:37:45 -0700

    added support for house plants

--- server/__init__.py
+++ server/__init__.py
@@ -7,10 +7,36 @@ __author__ = "Jeremy Nelson, Timothy Semple"
 
 import datetime
 import os
+import rdflib
 from flask import Flask, render_template
 
 app = Flask(__name__)
-PROJECT_ROOT = os.path.split(os.path.abspath(__file__))[0]
+PROJECT_ROOT = os.path.split(os.path.dirname(os.path.abspath((__file__))))[0]
+KNOWLEGE_GRAPH = rdflib.Graph()
+KNOWLEGE_GRAPH.parse(os.path.join(PROJECT_ROOT, "data/current.ttl"),
+                     format='turtle')
+SCHEMA = rdflib.Namespace("http://schema.org/")
+
+@app.template_filter('get_label')
+def label_(entity_iri, lang_code):
+    for label in KNOWLEGE_GRAPH.subjects(subject=entity_iri,
+        predicate=rdflib.RDFS.label):
+        if label.lang == lang_code:
+            return label
+    return ''
+                                    
+
+@app.route("/plant/<path:uuid>")
+def plant(uuid):
+    plant_iri = rdflib.URIRef("http://garden.local/plant/{}".format(uuid))
+    class_ = KNOWLEGE_GRAPH.value(subject=plant_iri,
+                                  predicate=rdflib.RDF.type)
+    if class_ is None:
+        abort(404)
+    elif class_ != SCHEMA.HousePlant:
+        abort(401)
+    return render_template("plant.html",
+        plant=plant_iri)
 
 @app.route("/")
 def hub():
